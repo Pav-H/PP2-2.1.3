f98533e64949129d9ce7abe2024f0d0b6e97daad
Pav-H <p.khomutov@gmail.com>
2023-07-23 02:44:32 +0300

    ADD: Solution of task 2.1.3

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,22 @@
 package app.config;
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+	@Bean("dog") // Определение бина с именем "dog"
+	public Animal dog() {
+		return new Dog();
+	}
+
+	@Bean("timer")
+	public Timer timer(){
+		return new Timer();
+	}
 }
